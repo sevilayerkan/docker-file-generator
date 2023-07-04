27312d2e11b01feaba107cbf84a15ce209be6371
sevilayerkan <sevilayerkan2@gmail.com>
2023-07-04 16:28:23 +0300

    FEAT(web): final touches for compose generator

--- app.py
+++ app.py
@@ -1,50 +1,73 @@
 import glob
 import os
 import json
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
+from werkzeug.utils import secure_filename
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        json_files = request.files.getlist('json_files')
+        json_files = request.form.getlist('json_files')
+        uploaded_files = request.files.getlist('uploaded_files')
         output_dir = request.form.get('output_dir', './')
         output_filename = request.form.get(
             'output_filename', 'docker-compose.yml')
 
-        # Save the uploaded JSON files
-        json_directory = save_uploaded_files(json_files)
+        if not json_files and not uploaded_files:
+            flash("Please select or upload at least one JSON file.", "error")
+            return redirect('/')
 
-        generate_docker_compose(json_directory, output_dir, output_filename)
+        all_files = json_files + save_uploaded_files(uploaded_files)
+
+        if not any(file.endswith('.json') for file in all_files):
+            flash("No valid JSON files selected or uploaded.", "error")
+            return redirect('/')
+
+        generate_docker_compose(all_files, output_dir, output_filename)
         return redirect('/success')
 
-    return render_template('index.html')
+    json_directory = './json_files'
+    json_files = get_json_files(json_directory)
 
+    return render_template('index.html', json_files=json_files)
 
-def save_uploaded_files(files):
-    json_directory = './uploaded_jsons'
-    os.makedirs(json_directory, exist_ok=True)
-
-    for file in files:
-        file_path = os.path.join(json_directory, file.filename)
-        file.save(file_path)
-
-    return json_directory
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 
 def get_json_files(directory):
-    json_files = glob.glob(os.path.join(directory, '*.json'))
+    json_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.json'):
+                json_files.append(os.path.join(root, file))
     return json_files
 
 
-def generate_docker_compose(json_directory, output_dir, output_filename):
-    docker_compose = []
-    docker_compose.append("version: '3'\n\nservices:\n")
-    service_names = []
+def save_uploaded_files(files):
+    saved_files = []
+    for file in files:
+        if file.filename:
+            file_path = os.path.join(
+                app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_path)
+            saved_files.append(file_path)
+    return saved_files
 
-    json_files = get_json_files(json_directory)
+
+def generate_docker_compose(json_files, output_dir, output_filename):
+    docker_compose = []
+
+    docker_compose.append("version: '3'\n\nservices:\n")
+
+    service_names = []
 
     for json_file in json_files:
         try:
@@ -53,25 +76,23 @@ def generate_docker_compose(json_directory, output_dir, output_filename):
 
             for service_name, service_config in config.items():
                 if service_name in service_names:
-                    print(
-                        f"Warning: Duplicate service name '{service_name}' in '{json_file}'. Skipping.")
+                    flash(f"Warning: Duplicate service name '{service_name}' in '{json_file}'. Skipping.")
                     continue
 
                 service_names.append(service_name)
+
                 docker_compose.append(f"  {service_name}:\n")
-                docker_compose.append("    image: {}\n".format(
-                    service_config.get('image', '')))
+                docker_compose.append("    image: {}\n".format(service_config.get('image', '')))
                 docker_compose.append("    ports:\n")
                 for port in service_config.get('ports', []):
                     docker_compose.append(f"      - {port}\n")
-                docker_compose.append("    command: {}\n".format(
-                    service_config.get('command', '')))
+                docker_compose.append("    command: {}\n".format(service_config.get('command', '')))
                 docker_compose.append("\n")
         except FileNotFoundError:
-            print(f"Error: JSON file '{json_file}' not found.")
+            flash(f"Error: JSON file '{json_file}' not found.")
             return
         except json.JSONDecodeError:
-            print(f"Error: Invalid JSON format in '{json_file}'.")
+            flash(f"Error: Invalid JSON format in '{json_file}'.")
             return
 
     os.makedirs(output_dir, exist_ok=True)
@@ -80,18 +101,11 @@ def generate_docker_compose(json_directory, output_dir, output_filename):
     try:
         with open(output_path, "w") as file:
             file.writelines(docker_compose)
-        print(f"Docker Compose file '{output_path}' generated successfully!")
+        flash(f"Docker Compose file '{output_path}' generated successfully!")
     except IOError:
-        print(
-            f"Error: Failed to write Docker Compose file '{output_path}'. Check the output directory.")
+        flash(f"Error: Failed to write Docker Compose file '{output_path}'. Check the output directory.")
         return
 
 
-@app.route('/success')
-def success():
-    return "Docker Compose YAML generated successfully!"
-
-
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
